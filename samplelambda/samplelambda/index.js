19205efaa7ef3983a933b958b57f1be438fe1844
midorigreen <stitow14@gmail.com>
2017-12-15 20:56:46 +0900

    Fix comment out

--- samplelambda/samplelambda/index.js
+++ samplelambda/samplelambda/index.js
@@ -28,20 +28,20 @@ exports.handler = (event, context, callback) => {
         },
     });
 
-    //switch (event.httpMethod) {
-    //    case 'DELETE':
-    //        dynamo.deleteItem(JSON.parse(event.body), done);
-    //        break;
-    //    case 'GET':
-    //        dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
-    //        break;
-    //    case 'POST':
-    //        dynamo.putItem(JSON.parse(event.body), done);
-    //        break;
-    //    case 'PUT':
-    //        dynamo.updateItem(JSON.parse(event.body), done);
-    //        break;
-    //    default:
-    //        done(new Error(`Unsupported method "${event.httpMethod}"`));
-    //}
+    switch (event.httpMethod) {
+        case 'DELETE':
+            dynamo.deleteItem(JSON.parse(event.body), done);
+            break;
+        case 'GET':
+            dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
+            break;
+        case 'POST':
+            dynamo.putItem(JSON.parse(event.body), done);
+            break;
+        case 'PUT':
+            dynamo.updateItem(JSON.parse(event.body), done);
+            break;
+        default:
+            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
 };
