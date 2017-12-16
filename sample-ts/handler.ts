b59fa7513681e21f2ac679ab6197ee8c43f08ee7
tagasouichiro <stitow14@gmail.com>
2017-12-16 19:08:44 +0900

    Add dynamodb put

--- sample-ts/handler.ts
+++ sample-ts/handler.ts
@@ -1,8 +1,34 @@
 'use strict';
 
+import * as uuid from 'uuid'
+import {DynamoDB} from 'aws-sdk'
+
+const dynamoDb = new DynamoDB.DocumentClient()
+
 export function hello(event: any, context: any, callback: Function) {
   console.log(event)
-  callback(null, {
-    message: "Hello World!"
-  });
-};
+
+  const timestamp = new Date().getTime()
+  console.log(timestamp)
+  const param = {
+    TableName: 'sample',
+    Item: {
+      id: uuid.v1(),
+      text: 'text',
+      createdAt: timestamp
+    }
+  }
+
+  dynamoDb.put(param, (error, result) => {
+    if (error) {
+      console.log(error)
+      callback(new Error("dynamodb put error"))
+      return
+    }
+
+    const response = {
+      statusCode: 200
+    }
+    callback(null, response)
+  })
+}
