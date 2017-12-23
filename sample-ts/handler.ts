f6900b5d3a48e4cf2a51c5fffb202f4547a3d268
tagasouichiro <stitow14@gmail.com>
2017-12-23 10:32:19 +0900

    Fix split function

--- sample-ts/handler.ts
+++ sample-ts/handler.ts
@@ -6,30 +6,49 @@ import { int } from 'aws-sdk/clients/datapipeline';
 
 const dynamoDb = new DynamoDB.DocumentClient()
 
+declare class Sample {
+  title: string
+  text: string
+  hoge: Hoge
+}
+
+declare class Hoge {
+  id: string
+  code: number
+}
+
 export function hello(event: Sample, context: any, callback: Function) {
   console.log(event)
+  dynamoDBGet(callback)
+  dynamoDBPut(event, callback)
+  dynamoDBUpdate(callback)
+}
 
-  const timestamp = new Date().getTime()
-  console.log(timestamp)
-
+function dynamoDBGet(callback: Function) {
   // Get
-  const gParam = {
+  const param = {
     TableName: 'sample',
     Key: {
       'id': '352345e0-e337-11e7-be4c-f7cf2d1f989a'
     }
   }
 
-  dynamoDb.get(gParam, (err, result) => {
+  dynamoDb.get(param, (err, result) => {
     if (err) {
       console.log(err)
       callback(new Error('dynamodb get error'))
       return
     }
     console.log(result)
+    const response = {
+      statusCode: 200
+    }
+    callback(null,response)
   })
-  
-  var upParam = {
+}
+
+function dynamoDBUpdate(callback: Function) {
+  var param = {
     TableName: 'sample',
     Key : {
       id : 'd4b46f90-e70f-11e7-8dd8-c5e92e1cb649'
@@ -37,23 +56,30 @@ export function hello(event: Sample, context: any, callback: Function) {
     ExpressionAttributeNames : {
       '#T': 'text',
       '#H': 'hoge_id',
+      '#C': 'hoge_code'
     },
     ExpressionAttributeValues: {
       ":newText": 'no event.json',
-      ":newHogeId": 'hogehoge'
+      ":newHogeId": 'hogehoge',
+      ":newHogeCode": 100
     },
-    UpdateExpression: 'SET #T = :newText, #H = :newHogeId'
+    UpdateExpression: 'SET #T = :newText, #H = :newHogeId, #C = :newHogeCode'
   }
-  dynamoDb.update(upParam, (error, result) => {
+  dynamoDb.update(param, (error, result) => {
     if (error) {
       console.log(error)
       return
     }
+    console.log(result)
     const response = {
       statusCode: 200
     }
     callback(null,response)
   })
+}
+
+function dynamoDBPut(event:Sample, callback:Function) {
+  const timestamp = new Date().getTime()
   // Post
   const param = {
     TableName: 'sample',
@@ -74,20 +100,11 @@ export function hello(event: Sample, context: any, callback: Function) {
       return
     }
 
+    console.log(result)
     const response = {
       statusCode: 200
     }
     callback(null, response)
   })
-}
-
-declare class Sample {
-  title: string
-  text: string
-  hoge: Hoge
-}
-
-declare class Hoge {
-  id: string
-  code: number
+  
 }
