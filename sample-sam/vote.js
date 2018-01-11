380dabae4d59aa31290ba954e395753dd7edc3f7
tagasouichiro <stitow14@gmail.com>
2018-01-11 23:22:40 +0900

    Add dynamo db post

--- sample-sam/vote.js
+++ sample-sam/vote.js
@@ -1,6 +1,26 @@
 
+var aws = require('aws-sdk')
+var dynamo = new aws.DynamoDB.DocumentClient({endpoint:'http://localhost:7777'})
+
 exports.handler = (event, context, callback) => {
   console.log(event)
   console.log(process.env.TABLE_NAME)
   callback(null, "Hello Local SAM")
+
+  var param = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      "id": event.id
+    }
+  }
+  dynamo.put(param, (err, result) => {
+    if (err) {
+      console.log(err)
+      callback(new Error("dynamodb put error"))
+      return
+    }
+
+    console.log(result)
+    callback(null, {status: 200})
+  })
 }
