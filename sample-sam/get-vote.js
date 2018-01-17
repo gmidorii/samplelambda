417cec062225be93f025763f35f0f3b8053bbe5d
tagasouichiro <stitow14@gmail.com>
2018-01-18 00:14:55 +0900

    Fix format and Add eslint

--- sample-sam/get-vote.js
+++ sample-sam/get-vote.js
@@ -1,40 +1,39 @@
-
-var AWS = require('aws-sdk')
+var AWS = require("aws-sdk");
 if (process.env.AWS_SAM_LOCAL) {
   AWS.config.update({
-    region: 'us-west-2',
+    region: "us-west-2",
     endpoint: process.env.LOCALURL
-  })
+  });
 }
 
-var dynamo = new AWS.DynamoDB.DocumentClient()
+var dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-  console.log(event)
-  console.log(event.body)
-  console.log(process.env.TABLE_NAME)
-  console.log(process.env.LOCALURL)
+  console.log(event);
+  console.log(event.body);
+  console.log(process.env.TABLE_NAME);
+  console.log(process.env.LOCALURL);
 
-  var body = JSON.parse(event.body)
+  var body = JSON.parse(event.body);
 
   var param = {
     TableName: process.env.TABLE_NAME,
     Key: {
-      "id": body.id
+      id: body.id
     }
-  }
+  };
 
   dynamo.get(param, (err, result) => {
     if (err) {
-      console.log(err)
-      callback(new Error("dynamodb get error"))
-      return
+      console.log(err);
+      callback(new Error("dynamodb get error"));
+      return;
     }
-    console.log(result)
+    console.log(result);
     const response = {
       response: result,
       statusCode: 200
-    }
-    callback(null, response)
-  })
-}
+    };
+    callback(null, response);
+  });
+};
--- sample-sam/post-vote.js
+++ sample-sam/post-vote.js
@@ -1,37 +1,36 @@
-
-var AWS = require('aws-sdk')
+var AWS = require("aws-sdk");
 if (process.env.AWS_SAM_LOCAL) {
   AWS.config.update({
-    region: 'us-west-2',
+    region: "us-west-2",
     endpoint: process.env.LOCALURL
-  })
+  });
 }
 
-var dynamo = new AWS.DynamoDB.DocumentClient()
+var dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-  console.log(event)
-  console.log(event.body)
-  var body = JSON.parse(event.body)
-  console.log(body.id)
-  console.log(process.env.TABLE_NAME)
-  console.log(process.env.LOCALURL)
+  console.log(event);
+  console.log(event.body);
+  var body = JSON.parse(event.body);
+  console.log(body.id);
+  console.log(process.env.TABLE_NAME);
+  console.log(process.env.LOCALURL);
 
   var param = {
     TableName: process.env.TABLE_NAME,
     Item: {
-      "id": body.id
+      id: body.id
     }
-  }
-  
+  };
+
   dynamo.put(param, (err, result) => {
     if (err) {
-      console.log(err)
-      callback(new Error("dynamodb put error"))
-      return
+      console.log(err);
+      callback(new Error("dynamodb put error"));
+      return;
     }
 
-    console.log(result)
-    callback(null, {statusCode: 200})
-  })
-}
+    console.log(result);
+    callback(null, { statusCode: 200 });
+  });
+};
