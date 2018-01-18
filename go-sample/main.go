a38194073d85ba97c1ef83b0510234e9d1c64416
midorigreen <stitow14@gmail.com>
2018-01-18 20:57:19 +0900

    Add Body parse

--- go-sample/main.go
+++ go-sample/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Body struct {
+	Word string
+	Code string
+}
+
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println(request)
+	var body Body
+	json.Unmarshal([]byte(request.Body), &body)
+	log.Println(body)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
