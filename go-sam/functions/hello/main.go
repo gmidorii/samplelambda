b9cce8d1288548c09ba2e6886d45083bc74ba34c
tagasouichiro <stitow14@gmail.com>
2018-02-22 06:42:33 +0900

    Update

--- go-sam/functions/hello/main.go
+++ go-sam/functions/hello/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Response struct {
+	Status  int16
+	Message string
+}
+
 func HelloHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println(request)
-	return events.APIGatewayProxyResponse{}, nil
+	res := Response{
+		Status:  http.StatusOK,
+		Message: "Hello!!",
+	}
+	s, err := json.Marshal(res)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, errors.New("json marshal error")
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(s),
+	}, nil
 }
 
 func main() {
